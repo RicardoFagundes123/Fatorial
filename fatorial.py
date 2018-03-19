b43e5f303a100324a72675f340a9ade7bf91439f
Ricardo de Lima Fagundes <rlf4@ug3c15.windows.cin.ufpe.br>
2018-03-19 15:56:51 -0300

    exception

--- fatorial.py
+++ fatorial.py
@@ -1,9 +1,10 @@
 def fat(n):
 	if ( not isinstance(n, (int, long)) ):
-	 	return False	
+	 	return TypeError("N must be an integer or long")	
 	elif (n <= 1):
 		return 1
 	else:
 		return n * fat(n - 1)
 
 print( fat(5) )
+print( fat(-5.2) )
